22394e90b2ec1a5df748ab0a4f86c352e9f96927
andreyz11288 <andreyz11288@gmail.com>
2020-10-28 14:09:48 +0200

    Изменение цвета работает

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+
+
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"]'),
+    stopBtn: document.querySelector('button[data-action="stop"]'),
+    body: document.querySelector('body')
+};
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+let intervalId = null;
+isActive = false;
+refs.startBtn.addEventListener('click', startEl)
+refs.stopBtn.addEventListener('click', stop)
+
+function startEl() {
+    if (isActive) { return }
+    isActive = true;
+   intervalId = setInterval(() => { 
+        refs.body.setAttribute("style", `background: ${colors[randomIntegerFromInterval(0, (colors.length - 1))]}`);
+   }, 1000)
+}
+
+function stop() {
+    clearInterval(intervalId);
+    isActive = false;
+};
+
+
+
+
+
+
+
+
